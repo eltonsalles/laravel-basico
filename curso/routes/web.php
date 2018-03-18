ef46ffc5a70a9808ecc73528819f728411db10a7
Elton Rodrigues <eltonrms.leite@hotmail.com>
2018-03-18 16:57:37 -0300

    Criado um novo controller com o padrão crud do Laravel usando o --resource.

--- curso/routes/web.php
+++ curso/routes/web.php
@@ -38,3 +38,4 @@ Route::get('/', function () {
 
 Route::get('/hello/{name?}', 'HelloController@index');
 Route::post('/hello/{name?}', 'HelloController@render');
+Route::resource('pages', 'Admin\PagesController');
